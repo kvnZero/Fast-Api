3e9c8af5bac73ffe62ae6f714366beec4cce6e56
abigeater <abigeater@163.com>
2020-06-26 14:48:29 +0800

    add 自动加载

--- index.php
+++ index.php
@@ -6,4 +6,8 @@ require(APP_PATH . 'fastapi/FastApi.php');
 
 $config = require(APP_PATH . 'config/config.php');
 
+spl_autoload_register(function ($class_name) {
+    require_once str_replace('\\','/',$class_name) . '.php';
+});
+
 (new fastapi\FastApi($config))->run();
