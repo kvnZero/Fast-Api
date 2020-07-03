5c12c559f7f01ea3b6f9e3ae2635a55f883fb61c
abigeater <abigeater@163.com>
2020-07-03 21:26:24 +0800

    增加了一些数据类型

--- fastapi/Db/Schema/Builder.php
+++ fastapi/Db/Schema/Builder.php
@@ -16,14 +16,44 @@ class Builder
         return $this->build_create_sql();
     }
 
-    public function field_string($name, $len = 10, $defalut='') {
+    private function field_base($type, $name, $len = 0, $defalut = ''){
         $field = [];
-        $field['type'] = 'VARCHAR';
+        $field['type'] = $type;
         $field['name'] = $name;
-        $field['len']  = $len;
+        $field['len']  = $len>0 ? $len : '';
+        $field['defalut'] = $defalut;
         $this->field[] = $field;
         return $this;
     }
+
+    public function field_string($name, $len = 10, $defalut='') {
+        return $this->field_base('VARCHAR', $name, $len, $defalut);
+    }
+
+    public function field_int($name, $defalut='') {
+        return $this->field_base('INT', $name, 0, $defalut);
+    }
+
+    public function field_big_int($name, $defalut='') {
+        return $this->field_base('BIGINT', $name, 0, $defalut);
+    }
+
+    public function field_date($name, $defalut='') {
+        return $this->field_base('DATE', $name, 0, $defalut);
+    }
+
+    public function field_datetime($name, $defalut='') {
+        return $this->field_base('DATETIME', $name, 0, $defalut);
+    }
+
+    public function field_longtext($name, $defalut='') {
+        return $this->field_base('LONGTEXT', $name, 0, $defalut);
+    }
+
+    public function field_text($name, $defalut='') {
+        return $this->field_base('TEXT', $name, 0, $defalut);
+    }
+
 
     private function build_create_sql() {
         $sql = '';
